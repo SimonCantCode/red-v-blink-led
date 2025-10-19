441b8feefab0820f1f262f90f1415cb790b338bf
Simon <simondepomian@gmail.com>
2025-10-19 20:43:31 +0200

    changed comments

--- blink.c
+++ blink.c
@@ -5,20 +5,23 @@
  * how they've mannaged to stay in buisness.
  * */
 
+// Memmory adresses from datasheet etc.
 #define GPIO_BASE_ADDR			0x10012000UL // Refference manual page 22. UL = unsigned long, best practice to specify.
-#define GPIO_INPUT_EN_OFFSET	0x04 // page 76
-#define GPIO_OUTPUT_EN_OFFSET	0x08 // page 76
-#define GPIO_OUTPUT_VAL_OFFSET	0x0C // page 76
+#define GPIO_INPUT_EN_OFFSET	0x04 // Refference manual page 76
+#define GPIO_OUTPUT_EN_OFFSET	0x08 // Refference manual page 76
+#define GPIO_OUTPUT_VAL_OFFSET	0x0C // Refference manual page 76
 #define GPIO_IOF_EN_OFFSET		0x38 // Not in the refference manual for some dumb reason.
-#define PIN						5 // Built in LED. Couln't fint any damn schematic for it though.
+#define PIN						5 // Built in LED pin. What we bitshift by
 
-// Has to be volatile to prevent "optimisation"
 typedef unsigned int uint32_t; // To avoid needing stdint.h
+
+// Declares pointers to all registers we'll use.
 volatile uint32_t* const GPIO_INPUT_EN		= (volatile uint32_t*)(GPIO_BASE_ADDR + GPIO_INPUT_EN_OFFSET);
 volatile uint32_t* const GPIO_OUTPUT_EN		= (volatile uint32_t*)(GPIO_BASE_ADDR + GPIO_OUTPUT_EN_OFFSET);
 volatile uint32_t* const GPIO_OUTPUT_VAL	= (volatile uint32_t*)(GPIO_BASE_ADDR + GPIO_OUTPUT_VAL_OFFSET);
 volatile uint32_t* const GPIO_IOF_EN		= (volatile uint32_t*)(GPIO_BASE_ADDR + GPIO_IOF_EN_OFFSET);
-// const to force it in flash instead of ram, we dont have .data in linker file.
+// Has to be volatile to prevent them being optimized away by the compiler.
+// Has to be const to force it in flash (.text) instead of ram (.data) since we don't initialize .data. This bug took a while to find. You can alternernatively just initialize .data in start.s, its defined in the linker file.
 
 // Low effort delay function
 void delay(unsigned int cycles) {
@@ -38,10 +41,11 @@ int main() {
 	// Enable output for pin
 	*GPIO_OUTPUT_EN |= (1 << PIN); 
 
+	// Main loop
 	while(1) {
 		// Turn on led
 		*GPIO_OUTPUT_VAL |= (1 << PIN);
-		delay(1000000);
+		delay(1000000); // Roughly half a second delay
 
 		// Turn off led
 		*GPIO_OUTPUT_VAL &= ~(1 << PIN);
